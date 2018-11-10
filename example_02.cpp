6831704abe1a1d0f534abcd98a544c3d410d32fa
Matic Drobež <matic.drobez.extern@sixt.de>
2018-11-10 09:59:03 +0100

    Third commit with second example.

---
+++ example_02.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main()
+{
+	uint32_t localVariableA = 10;
+	uint32_t localVariableB = 20;
+	uint32_t localVariableC = localVariableA + localVariableB;
+
+	std::cout << "Suma is: " << localVariableC << std::endl;
+
+	return 0;
+}
