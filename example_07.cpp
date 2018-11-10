d8b2fdf38a6a368a807443c4f71d7135d0f02564
Dr_Matic <matic.drobez@gmail.com>
2018-11-10 18:07:29 +0100

    Vector example.

---
+++ example_07.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+int main()
+{
+    std::vector<int> vi;
+    for(int i = 0; i<10; ++i)
+    {
+        vi.push_back(i);
+    }
+
+    for(auto item:vi)
+    {
+        std::cout << item << "|";
+    }
+
+    std::cout << std::endl;
+
+
+    std::vector<std::string> vectorOfStrings;
+
+    for(int i = 0; i < 4; i++)
+    {
+        std::string word;
+        std::cin >> word;
+        vectorOfStrings.push_back(word);
+    }
+
+    for(auto word:vectorOfStrings)
+    {
+        std::cout << word << "||";
+    }
+
+    std::cout << std::endl;
+}
