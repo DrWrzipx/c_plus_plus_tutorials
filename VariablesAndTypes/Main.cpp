8220a7a5d3ff8e0f24d2496a4b95a36813b80c74
Dr_Matic <matic.drobez@gmail.com>
2018-11-14 20:38:48 +0100

    Add some new type auto.

--- VariablesAndTypes/Main.cpp
+++ VariablesAndTypes/Main.cpp
@@ -17,5 +17,15 @@ int main()
 	std::cout << "i1 = " << i1 << std::endl;
 
 
+	auto a1 = bool;
+	auto a2 = 234;
+	auto a3 = "Hello";
+	auto a4 = 3.45;
+
+	std::cout << "a1 = " << a1 << std::endl;
+	std::cout << "a2 = " << a2 << std::endl;
+	std::cout << "a3 = " << a3 << std::endl;
+	std::cout << "a4 = " << a4 << std::endl;
+
 	return 0;
 }
